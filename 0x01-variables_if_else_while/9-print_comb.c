598219e3259f7087d8e24d8806c34c01a43bd29c
Yamza108 <yamkelafukuse@gmail.com>
2023-02-17 19:55:02 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 100; a++)
+	{
+		putchar(a + '0');
+		if (a < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
