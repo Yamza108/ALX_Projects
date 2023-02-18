e4a7f35f639b48cc291384e37659bb8a64955721
Yamza108 <yamkelafukuse@gmail.com>
2023-02-17 21:03:13 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
-	putchar(',');
+	putchar(' ');
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
 	if (p == 98 && q == 99)
