976ffe13281e0be59f16b580386d9bd59e481eb8
Yamza108 <yamkelafukuse@gmail.com>
2023-02-22 06:15:09 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - check the code.
- *
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase.
+ * followed by a new line.
  * Return: Always 0.
  */
 
