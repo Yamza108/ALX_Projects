624c6e4074814a2767f09ecc7e6ea2c7158f0541
Yamza108 <yamkelafukuse@gmail.com>
2023-02-23 13:07:05 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 5-main.c 5-more_numbers.c -o 5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * more_numbers - Print 10 times the numbers since 0 up to 14
+ * Return: 10 times of the numbers since 0 up to 14
+ */
+
+void more_numbers(void)
+
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 0)
+	{
+	putchar((y / 10) + '0');
+	}
+	putchar((y % 10) + '0');
+	}
+	putchar('\n');
+	}
+}
