a8efe89469246bd857988a375b09bfc3faac6d63
Yamza108 <yamkelafukuse@gmail.com>
2023-02-23 06:16:58 -0800

    Duck method of debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
-nclude "main.h"
-
+#include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -7,13 +6,10 @@ nclude "main.h"
 */
 
 int main(void)
-
 {
-
 	int i;
 
-	i = 0;
+	i = 98;
 	positive_or_negative(i);
-
 	return (0);
 }
