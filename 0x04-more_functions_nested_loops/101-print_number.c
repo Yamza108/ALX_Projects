dff1de0b6516d38e8fbf8c715ceb2dfea5408870
Yamza108 <yamkelafukuse@gmail.com>
2023-02-24 11:03:40 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,26 +3,33 @@
 #include <unistd.h>
 
 /**
- * print_number - Prints an integer
- * @n: The integer to prints
- * Return: Nothing!
+ * print_number - Prints an integer using _putchar
+ * @n: The integer to print
  */
 
 void print_number(int n)
-
 {
-	unsigned int k;
-
-	k = n;
+	int divisor;
 
 	if (n < 0)
 	{
-	n *= -1;
-	k = n;
 	putchar('-');
+	n = -n;
 	}
-	k /= 10;
-	if (k != 0)
-	print_number(k);
-	putchar((unsigned int) n % 10 + '0');
+
+	divisor = 1;
+
+	while (n / divisor >= 10)
+	divisor *= 10;
+
+	while (divisor != 0)
+	{
+	int digit = n / divisor;
+
+	putchar(digit + '0');
+	n %= divisor;
+	divisor /= 10;
+	}
 }
+
+
