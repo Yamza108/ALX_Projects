137bbbb27463885f5f46e5ff833c2840891f1a38
Yamza108 <yamkelafukuse@gmail.com>
2023-03-06 01:41:02 -0800

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, n, value, check;
+
+	value = 0;
+
+	for (i = 0; s[i] != '\0'; i++){
+	check = 0;
+
+	for (n = 0; accept[n] != '\0'; n++)
+	{
+		if (accept[n] == s[i])
+	{
+	value++;
+	check = 1;
+	}
+	}
+	}
+	if (check == 0)
+	
+return (0);
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - Entry point
+ * @: input
+ * @accept: input
+ * Return: Always 0.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (n = 0; accept[n] != '\0'; n++)
+	{
+	if (s[i] == accept[n])
+	return (s + i);
+	}
+	}
+	return (NULL);
+}
