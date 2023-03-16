afe2f8c70f9bd4c6f42c53aebdee724f0208549c
Yamza108 <yamkelafukuse@gmail.com>
2023-03-16 09:55:07 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * _calloc - ...
+ * @nmemb: number of members
+ * @size: size
+ *
+ * Return: Nothing.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, l = 0;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+
+	l = nmemb * size;
+	p = malloc(l);
+
+	if (p == NULL)
+	return (NULL);
+
+	while (i < l)
+	{
+	p[i] = 0;
+	i++;
+	}
+	return (p);
+}
