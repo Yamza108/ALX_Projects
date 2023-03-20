90bd8736f2eaefb6e90fda14b0650b06dea946d3
Yamza108 <yamkelafukuse@gmail.com>
2023-03-20 08:20:46 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * free_dog - frees dogs
+ * @d: The dog to be freed
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
