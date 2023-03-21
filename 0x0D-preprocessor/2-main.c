9c78be8b8371893105c0fdfdfe8b2a7066a0b4c8
Yamza108 <yamkelafukuse@gmail.com>
2023-03-20 23:28:31 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the nmaae of the file program was compiled from
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
