42e8592ec4eeba48406053158f96b4cdcc14e467
Yamza108 <yamkelafukuse@gmail.com>
2023-04-09 00:38:11 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,7 +18,7 @@ char *create_buffer(char *file)
 	if (buffer == NULL)
 	{
 	dprintf(STDERR_FILENO,
-		"Error: can;t write to %s\n", file);
+		"Error: Can't write to %s\n", file);
 	exit(99);
 	}
 	return (buffer);
@@ -37,7 +37,7 @@ void close_file(int fd)
 
 	if (c == -1)
 	{
-	dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 	exit(100);
 	}
 }
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
 	if (from == -1 || r == -1)
 	{
 	dprintf(STDERR_FILENO,
-		"Error: can't read from file %s\n", argv[1]);
+		"Error: Can't read from file %s\n", argv[1]);
 	free(buffer);
 	exit(98);
 	}
@@ -81,7 +81,7 @@ int main(int argc, char *argv[])
 	if (to == -1 || w == -1)
 	{
 	dprintf(STDERR_FILENO,
-		"Error: can't write to %s\n", argv[2]);
+		"Error: Can't write to %s\n", argv[2]);
 	free(buffer);
 	exit(99);
 	}
